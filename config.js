91182950724c5579e08d3f6e24bb1075f9b1a731
Quan Li <qli@caregility.com>
2021-11-12 23:05:33 -0500

    Updates

---
+++ config.js
@@ -0,0 +1,7 @@
+appConfig = {
+    RedirectUri: "https://quanlicaregility.github.io/epic",
+    // RedirectUri: 'http://74.105.141.196:8080',
+    ClientId: 'be1aa87b-71b8-41de-ac0d-3ed78c558899',
+    OAuthBaseUri: 'https://apporchard.epic.com/interconnect-aocurprd-oauth/oauth2',
+    FhirBaseUri: 'https://apporchard.epic.com/interconnect-aocurprd-oauth/api/FHIR/R4'
+}
