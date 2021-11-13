f7705a8526d9fd97e82e61041240c793e36b6fc6
Quan Li <qli@caregility.com>
2021-11-12 23:14:34 -0500

    Updates

--- bundle.js
+++ bundle.js
@@ -62960,7 +62960,7 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var App = function App() {
-  return React.createElement(_reactRouterDom.BrowserRouter, null, React.createElement(_reactRouterDom.Routes, null, React.createElement(_reactRouterDom.Route, {
+  return React.createElement(_reactRouterDom.HashRouter, null, React.createElement(_reactRouterDom.Routes, null, React.createElement(_reactRouterDom.Route, {
     path: '/epic',
     element: React.createElement(_launch.LaunchComponent, {
       params: window.location.search
